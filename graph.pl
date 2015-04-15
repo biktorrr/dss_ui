f302f617a5999304e8b0739129f49bfbec153f08
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2015-04-15 13:11:14 +0200

    Initial version:

---
+++ graph.pl
@@ -0,0 +1,29 @@
+:- module(dss_graph, []).
+:- use_module(cliopatria(hooks)).
+:- use_module(library(semweb/rdf_db)).
+:- use_module(library(semweb/rdfs)).
+
+create_inverse_properties :-
+	forall(rdf(P, owl:inverseOf, P2),
+	       rdf_set_predicate(P, inverse_of(P2))).
+
+:- initialization create_inverse_properties.
+
+:- rdf_meta
+	dss_context_predicate(r, r).
+
+dss_context_predicate(_, mdb:bestemming).
+dss_context_predicate(_, mdb:has_aanmonstering).
+dss_context_predicate(_, mdb:persoon).
+dss_context_predicate(_, mdb:rang).
+dss_context_predicate(_, mdb:ship).
+dss_context_predicate(_, mdb:ligplaats).
+dss_context_predicate(_, mdb:thuishaven).
+dss_context_predicate(_, mdb:vertrekhaven).
+dss_context_predicate(_, mdb:woonplaats).
+dss_context_predicate(_, dss:has_schiptype).
+dss_context_predicate(_, mdb:has_person_contract).
+dss_context_predicate(_, mdb:has_contract).
+
+cliopatria:context_predicate(S, P) :-
+	dss_context_predicate(S, P).
---
+++ local.pl
@@ -0,0 +1,6 @@
+:- use_module(library(semweb/rdf_db)).
+
+:- rdf_register_prefix(mdb, 'http://purl.org/collections/nl/dss/mdb/').
+:- rdf_register_prefix(dss, 'http://purl.org/collections/nl/dss/').
+
+:- use_module(graph).

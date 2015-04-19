1df874e42a10b656563f986f9d6ac855e5d8a8f0
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2015-04-19 09:48:23 +0200

    Register namespaces and attach the library

--- local.pl
+++ local.pl
@@ -1,6 +1,9 @@
 :- use_module(library(semweb/rdf_db)).
+:- use_module(library(semweb/rdf_library)).
 
 :- rdf_register_prefix(mdb,    'http://purl.org/collections/nl/dss/mdb/').
+:- rdf_register_prefix(gzmvoc, 'http://purl.org/collections/nl/dss/gzmvoc/').
+:- rdf_register_prefix(das,    'http://purl.org/collections/nl/dss/das/').
 :- rdf_register_prefix(dss,    'http://purl.org/collections/nl/dss/').
 :- rdf_register_prefix(schema, 'http://schema.org/').
 
@@ -8,3 +11,5 @@
 :- use_module(localview).
 :- use_module(dss_queries).
 :- use_module(label).
+
+:- rdf_attach_library(.).
